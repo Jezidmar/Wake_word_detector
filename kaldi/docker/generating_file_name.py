9b38d86e17722622fc5027df180c95118e70be17
Jezidmar <102309599+Jezidmar@users.noreply.github.com>
2024-06-30 16:53:09 +0200

    Delete kaldi/docker directory

--- kaldi/docker/generating_file_name.py
+++
@@ -1,6 +0,0 @@
-import uuid
-
-
-def generate_filename(prefix="data", extension=".txt"):
-    unique_id = uuid.uuid4()
-    return f"{prefix}_{unique_id}{extension}"
--- kaldi/docker/loading_model.py
+++
@@ -1,7 +0,0 @@
-import onnxruntime as ort
-
-
-def load_model(model_path):
-    # Load the ONNX model
-    session = ort.InferenceSession(model_path)
-    return session
